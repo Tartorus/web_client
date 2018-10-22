332974a0b0865cfd74d0fb8f17beff38e5b36c7f
iliyas <iliyas.basharov@yango.pro>
2018-10-22 12:40:36 +0300

    Дописан переключатель активы-идеи-профиль.

--- src/components/home_page/HomePage.jsx
+++ src/components/home_page/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { request, history } from '../../_helpers';
 import { loadAccounts } from './actions';
@@ -6,14 +7,21 @@ import { urlsConstants } from '../../_constants';
 import { Ideas } from './Ideas';
 
 const IDEAS = 'ideas';
-const ASSETS = 'assets'
+const ASSETS = 'assets';
+const PROFILE = 'profile';
+
+const headerTitle = {};
+headerTitle[IDEAS] = 'Торговые Идеи'
+headerTitle[ASSETS] = 'Мои активы'
+headerTitle[PROFILE] = 'Мой профиль'
+
 
 class HomePage extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             curLink: IDEAS,
-
+            title: headerTitle[IDEAS],
         }
     }
 
@@ -33,30 +41,57 @@ class HomePage extends React.Component {
     }
 
     handleClick(curLink){
-        return e => { this.setState({curLink}) }
+        return e => {
+            console.log(headerTitle, curLink);
+            let title = headerTitle[curLink];
+            console.log(title);
+            this.setState({curLink: curLink, title: title})
+        }
+    }
+
+    switcherCss(link){
+        return classNames({
+            'hp-header__switcher': true,
+            'switcher_selected': this.state.curLink === link
+        })
     }
 
   render(){
-    return(
-      <div>
-        <h1>HomePage</h1>
-        <div className="container">
-            <div className='row'>
-                <div className='col-md-4 col-md-offset-5'>
-                    <div className="btn-group btn-group-lg" role="group">
-                            <button type="button" className={"btn btn-default " + (this.state.curLink === IDEAS ? 'btn-primary' : '')} onClick={this.handleClick(IDEAS)} >Идеи</button>
-                            <button type="button" className={"btn btn-default " + (this.state.curLink === ASSETS ? 'btn-primary' : '')} onClick={this.handleClick(ASSETS)}>Активы</button>
+      return(
+          <div>
+            <div className='container'>
+                <div className='row hp-header'>
+                    <div className='container'>
+                        <div className='row hp-header__title'>{this.state.title}</div>
+                        <div className='row'>
+                            <div className='col-md-4' onClick={this.handleClick(ASSETS)}>
+                                <div className={this.switcherCss(ASSETS)}>
+                                    <span className="glyphicon glyphicon-briefcase" aria-hidden="true"></span>
+                                    <p>Активы</p>
+                                </div>
+                             </div>
+                            <div className='col-md-4' onClick={this.handleClick(IDEAS)}>
+                                <div className={this.switcherCss(IDEAS)}>
+                                    <span className="glyphicon glyphicon-list" aria-hidden="true"></span>
+                                    <p>Идеи</p>
+                                </div>
+                            </div>
+                            <div className='col-md-4' onClick={this.handleClick(PROFILE)}>
+                                <div className={this.switcherCss(PROFILE)}>
+                                    <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                    <p>Профиль</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div className='row content'>
+                { this.state.curLink === IDEAS && <Ideas/> }
                 </div>
             </div>
         </div>
-
-        { this.state.curLink === IDEAS && <Ideas/> }
-        { this.state.curLink === ASSETS && <div>ASSETS</div> }
-
-      </div>
     );
-  }
+    }
 }
 
 
